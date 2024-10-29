dcfef7490f8a8c0612aa5e06463cab2d49e2a0f9
Rohit Singh <jerryy1803+s@gmail.com>
2024-10-30 02:02:21 +0530

    commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name='I Hear You (An audio-speech-to-sign-language-converter)',
     version='0.1.0',
     description='HackOdisha4 Project',
-    author='Rohit Singh',
+    author='Gourav Rout',
     url='',
     packages=setuptools.find_packages(),
     setup_requires=['nltk', 'joblib','click','regex','sqlparse','setuptools'],
